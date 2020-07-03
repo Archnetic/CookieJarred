6743bf579bc1a5043418390d992fb746a5030dfb
Chaselose <chasemlose@gmail.com>
2020-07-03 11:11:56 -0400

    Tabs
    Gather current loading tab

--- jarred.js
+++ jarred.js
@@ -9,14 +9,29 @@
 
 }*/
 
-function jarCookie(){
-    window.onload = eatmyass()
-    
-}
-function eatmyass(){
-    console.log("i ate it bitch")
-    getCurrTab.querySelector(".cc-banner")
+
+/*function jarCookie(tabs){
+    for (let tab of tabs) {
+        // tab.url requires the `tabs` permission
+        console.log(tab.url);
+      }
 }
 
 var getCurrTab = browser.tabs.query({active: true, currentWindow: true});
+browser.webNavigation.onCompleted.addListener(jarCookie);*/
+
+function logTabs(tabs) {
+  // tabs[0].url requires the `tabs` permission
+  console.log(tabs[0].url);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function jarCookie(){
+    let querying = browser.tabs.query({currentWindow: true, active: true});
+    querying.then(logTabs, onError);
+    
+}
 browser.webNavigation.onCompleted.addListener(jarCookie);
