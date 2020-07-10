7a7eb4ceae161fa78c8c4db145364610fcccc3af
Chaselose <chasemlose@gmail.com>
2020-07-09 20:36:30 -0400

    Computer Change

--- content-script.js
+++ content-script.js
@@ -1,2 +1,5 @@
 //this verified makes it to every page and executes
+// test site https://adamsavage.com
+// the command does remove the banner but the automation works too fast and executes before the page loads
 document.querySelector('.cc-banner').remove()
+document.querySelector('.cc-window').remove()
