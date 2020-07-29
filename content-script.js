c03a3e02d5de7e82c9a856de41dc6f8628ff4521
Chase Lose <chasemlose@gmail.com>
2020-07-28 23:41:51 -0400

    Version 1
    Unrevised version 1

--- content-script.js
+++ content-script.js
@@ -6,14 +6,21 @@ ask for cookie consent as soon as the site is completed will be removed first.
 //test site for delayed cookie removeal https://adamsavage.com
 //test site for standard onCompleted cookie removeal https://filewave.com
 
-if(document.querySelector('.cc-banner') == null){
-    setTimeout(function() { removeConsent(); }, 0500)
-}
-else{
-    removeConsent()
+var idarray = ['.ccc-banner', '.cc-banner', '.ccpaCookieBanner', '.sc-fzqARJ gPfttN']
+
+for(i = 0; i<idarray.length; i++){
+    if(document.querySelector(idarray[i]) == null){
+        setTimeout(function(){removeConsent(idarray[i]);}, 1000)
+    }
+    else{
+        removeConsent(idarray[i])
+    }
 }
 
-function removeConsent(){
-    let array = ['.cc-banner']
-    document.querySelector(array).remove()
+function removeConsent(temp){
+    document.querySelector(temp).remove()
+}
+
+function addNewId(){
+
 }
